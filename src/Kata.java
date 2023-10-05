8262981ff919c1748b525de0be2277a1f82863dd
Rodrigo <rss.999999999@gmail.com>
2023-10-05 10:05:12 -0300

    first draft

--- src/Kata.java
+++ src/Kata.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class squareSum {
+/**
+
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+*/
+
+public class Kata {
+    public static int squareSum(int[] n) {
+        int sum = 0;
+        for (int i : n) {
+            sum += i * i;
+        }
+        return sum;
+
+    }
 }
--- tests/Tests.java
+++ tests/Tests.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.stream.IntStream;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -10,5 +13,16 @@ public class Tests
         assertEquals(5, Kata.squareSum(new int[] {1,2}));
         assertEquals(50, Kata.squareSum(new int[] {5,-3,4}));
         assertEquals(0, Kata.squareSum(new int[] {}));
+    }
+
+    @Test
+    public void testRandom()
+    {
+        Random rnd = new Random();
+        for(int t=0; t<100; ++t) {
+            int[] array = IntStream.generate( () -> rnd.nextInt(100) - 50).limit(rnd.nextInt(20) + 30).toArray();
+            int sum = IntStream.of(array).map(i->i*i).sum();
+            assertEquals(sum, Kata.squareSum(array));
+        }
     }
 }
